30c4b2fae1b1ba265284db36b3cadbd0cb12ba70
skylor727 <skylor1997@hotmail.com>
2022-03-01 16:33:39 -0700

    fix user model and add jwt

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,10 +1,27 @@
-const create = (req, res) => {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  });
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+
+createJWT = (user) => {
+  return jwt.sign(
+    //data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+};
+
+create = async (req, res) => {
+  try {
+    //Add user to DB
+    const user = await User.create(req.body);
+    //token will be a string
+    const token = createJWT(user);
+    //Serialize a string
+    res.json(token);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 };
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,45 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
+
 const SALT_ROUNDS = 6;
 
-const userSchema = new Schema(
-  {
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: {
     type: String,
-    required: true,
-    email: {
-      type: String,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      trim: true,
-      minlength: 3,
-      required: true,
-    },
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true
   },
-  {
-    timestamps: true,
-    toJSON: {
-      transform: function (doc, ret) {
-        delete ret.password;
-        return ret;
-      },
-    },
+  password: {
+    type: String,
+    trim: true,
+    minlength: 3,
+    required: true
   }
-);
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function(doc, ret) {
+      delete ret.password;
+      return ret;
+    }
+  }
+});
 
-userSchema.pre("save", async function (next) {
-  //'this' is the user document | Mongoose cannot override
-  if (!this.isModified("password")) return next();
-  //update the password with the computed hash
-  this.password = await bcrypt.hash(
-    this.password,
-    SALT_ROUNDS
-  ); /*Now provide salt rounds | Up by 1 = 1 pow(2) */
+userSchema.pre('save', async function(next) {
+  // 'this' is the use document
+  if (!this.isModified('password')) return next();
+  // update the password with the computed hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
   return next();
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
