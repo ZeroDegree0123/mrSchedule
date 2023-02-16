c8e9884fc19ed8fc55a36ddcbb34e63a456f8fd8
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-02-16 12:49:36 -0800

    first commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,6 +9,7 @@ export default function HomePage() {
                 </section>
                 <section>
                     <h1>SERVICES</h1>
+                    <h1>hello</h1>
                 </section>
                 <section>
                     <h1>REFERENCES</h1>
