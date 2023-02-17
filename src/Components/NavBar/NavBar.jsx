7010a7fed85f87d1e55146b5c1c4288abec1e79a
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-02-16 19:35:40 -0800

    navbar checkpoint about finished open state

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,14 +8,31 @@ export default function NavBar({ user, setUser }) {
     setUser(null);
   }
   return (
-    <nav className="nav-bar">
-      
-            <Link id="logout" className="nav-links" onClick={handleLogOut} to="/">
-              Logout
-            </Link>
-            <h2 className="user-name">WELCOME {user.name.toUpperCase()}!</h2>
-            {/* <Link className="nav-links" to="/login">Login</Link> */}
-          
+    <nav className="nav-bar open">
+        <div className="nav-toggle">&#62;</div>
+        {/* <button className="nav-toggle">X</button> */}
+      <div className="user-info">
+        <img src="https://imgur.com/XSenBva.png" alt="" />
+        <h2 className="user-name">{user.name.toUpperCase()}</h2>
+      </div>
+      <div className="nav-titles">
+          Navigation
+      </div>
+      <div className="nav-list">
+        <Link id="logout" className="nav-item active" onClick={handleLogOut} to="/">Logout</Link>
+        <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
+        <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
+        <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
+        {/* <Link className="nav-links" to="/login">Login</Link> */}
+      </div>
+      <hr />
+      <div className="nav-titles">
+          Support
+      </div>
+      <div className="nav-list">
+        <Link id="logout" className="nav-item active" onClick={handleLogOut} to="/">Logout</Link>
+        <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
+      </div>
     </nav>
   );
 }
