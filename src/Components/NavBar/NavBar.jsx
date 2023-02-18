91ae8a42f792b8d485b1c9cb956809d0f66455bb
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-02-17 20:23:43 -0800

    styles navbar open and close need to code the functionality

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -7,17 +7,35 @@ export default function NavBar({ user, setUser }) {
     userService.logOut();
     setUser(null);
   }
+
+  const navbar = document.querySelector('.nav-bar')
+  const toggle = document.querySelector('.nav-toggle')
+  // const navItems = document.querySelector('.nav-list')
+
+  // toggle.addEventListener('click', () => {
+  //   if (navbar.className === '.nav-bar') {
+  //     // sidebar
+  //   }
+  // })
+
   return (
-    <nav className="nav-bar open">
+    <nav className="nav-bar">{/* Effected */}
+      
         <div className="nav-toggle">&#62;</div>
-        {/* <button className="nav-toggle">X</button> */}
+
+      {/* Effected */}
       <div className="user-info">
         <img src="https://imgur.com/XSenBva.png" alt="" />
         <h2 className="user-name">{user.name.toUpperCase()}</h2>
       </div>
+
+      {/* Effected */}
       <div className="nav-titles">
           Navigation
       </div>
+
+
+      {/* Effected */}
       <div className="nav-list">
         <Link id="logout" className="nav-item active" onClick={handleLogOut} to="/">Logout</Link>
         <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
@@ -26,10 +44,15 @@ export default function NavBar({ user, setUser }) {
         {/* <Link className="nav-links" to="/login">Login</Link> */}
       </div>
       <hr />
+
+      {/* Effected */}
       <div className="nav-titles">
           Support
       </div>
-      <div className="nav-list">
+
+
+      {/* Effected */}
+      <div className="nav-list"> 
         <Link id="logout" className="nav-item active" onClick={handleLogOut} to="/">Logout</Link>
         <Link id="logout" className="nav-item" onClick={handleLogOut} to="/">Logout</Link>
       </div>
